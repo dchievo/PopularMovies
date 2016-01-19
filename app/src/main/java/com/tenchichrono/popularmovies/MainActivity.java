b99515c8a2de6dabca34a20897dd0dfe44a59f01
dchi <dchievo@yahoo.com>
2016-01-19 15:53:16 -0800

    Added movie images. Working on passing values via intent to movie info activity

--- app/src/main/java/com/tenchichrono/popularmovies/MainActivity.java
+++ app/src/main/java/com/tenchichrono/popularmovies/MainActivity.java
@@ -2,6 +2,7 @@ package com.tenchichrono.popularmovies;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -11,14 +12,10 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MainActivity extends AppCompatActivity {
 
     public static final String MOVIE = "";
     private ImageAdapter mImageAdapter;
-    private Map <String,String> mMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +32,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 ImageView imageView = (ImageView) findViewById(R.id.movie_title_image_view);
-                ImageView movie = (ImageView) mImageAdapter.getItem(position);
-                imageView.setImageDrawable(movie);
-
                 Intent intent = new Intent(getApplication().getApplicationContext(), MovieInfo.class);
-                intent.putExtra(MOVIE, movieName);
+                intent.putExtra(MOVIE, (Parcelable) imageView);
                 startActivity(intent);
             }
         });
@@ -71,9 +65,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void setMovies()
-    {
-        mMovies = new HashMap<>();
-            mMovies.put("movies_01","Inspired by true events, THE REVENANT is an immersive and visceral cinematic experience capturing one man's epic adventure of survival and the extraordinary power of the human spirit. In an expedition of the uncharted American wilderness, legendary explorer Hugh Glass (Leonardo DiCaprio) is brutally attacked by a bear and left for dead by members of his own hunting team. In a quest to survive, Glass endures unimaginable grief as well as the betrayal of his confidant John Fitzgerald (Tom Hardy). Guided by sheer will and the love of his family, Glass must navigate a vicious winter in a relentless pursuit to live and find redemption. THE REVENANT is directed and co-written by renowned filmmaker, Academy Award (R) winner Alejandro G. Iñárritu (Birdman, Babel).");
-    }
 }
--- app/src/main/java/com/tenchichrono/popularmovies/MovieInfoFragment.java
+++ app/src/main/java/com/tenchichrono/popularmovies/MovieInfoFragment.java
@@ -2,16 +2,22 @@ package com.tenchichrono.popularmovies;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MovieInfoFragment extends Fragment {
+
+    private Map<String,String> mMovies;
 
     public MovieInfoFragment() {
     }
@@ -23,10 +29,16 @@ public class MovieInfoFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_movie_info, container, false);
 
         Intent intent = getActivity().getIntent();
-        String movieTitle = intent.getStringExtra(MainActivity.MOVIE) + "";
+        ImageView imageView = (ImageView) view.findViewById(R.id.movie_title_image_view);
 
-        TextView textView = (TextView) view.findViewById(R.id.movie_title_text_view);
-        textView.setText(movieTitle);
+        imageView.setImageDrawable(intent.getParcelableExtra(MainActivity.MOVIE));
         return view;
+    }
+
+
+    public void setMovies()
+    {
+        mMovies = new HashMap<>();
+        mMovies.put("movies_01", "Inspired by true events, THE REVENANT is an immersive and visceral cinematic experience capturing one man's epic adventure of survival and the extraordinary power of the human spirit. In an expedition of the uncharted American wilderness, legendary explorer Hugh Glass (Leonardo DiCaprio) is brutally attacked by a bear and left for dead by members of his own hunting team. In a quest to survive, Glass endures unimaginable grief as well as the betrayal of his confidant John Fitzgerald (Tom Hardy). Guided by sheer will and the love of his family, Glass must navigate a vicious winter in a relentless pursuit to live and find redemption. THE REVENANT is directed and co-written by renowned filmmaker, Academy Award (R) winner Alejandro G. Iñárritu (Birdman, Babel).");
     }
 }
